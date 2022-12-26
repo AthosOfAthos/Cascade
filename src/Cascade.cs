f1e5c9caec178262c90ee24c0318e15a4ec7b9a8
Redfar <redfarofket@gmail.com>
2022-12-26 11:33:38 -0600

    Added some basic managers for code seperation

--- src/Cascade.cs
+++ src/Cascade.cs
@@ -1,6 +1,10 @@
+using Cascade.src.Managers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Cascade;
 
@@ -10,36 +14,47 @@ public class Cascade: Game {
         game.Run();
     }
     
-    private GraphicsDeviceManager graphics;
+    private GraphicsDeviceManager _graphics;
+
+    private IEnumerable<IManager> _managers;
     
 	public Cascade() {
-        graphics = new GraphicsDeviceManager(this);
+        _graphics = new GraphicsDeviceManager(this);
 		Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        //Manager content, this should be redone with dependency injection
+        _managers = new List<IManager> { 
+            new EntityManager(),
+            new WorldManager()
+        };
 	}
 
     protected override void Initialize()
     {
+        foreach (IManager manager in _managers) { manager.Initialize(this); }
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
+        foreach (IManager manager in _managers) { manager.LoadContent(); }
         base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-		
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) { Exit(); }
+        foreach (IManager manager in _managers) { manager.Update(gameTime); }
+        
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-		GraphicsDevice.Clear(Color.Gray);
-		
+        GraphicsDevice.Clear(Color.Gray);
+        foreach (IManager manager in _managers) { manager.Draw(gameTime); }
+        
         base.Draw(gameTime);
     }
 }
