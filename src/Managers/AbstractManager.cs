1a973101afce4e0ecee9c9c7c9ff3114569a1af4
Redfar <redfarofket@gmail.com>
2022-12-26 11:34:18 -0600

    Added manager examples

---
+++ src/Managers/AbstractManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cascade.src.Managers
+{
+    internal abstract class AbstractManager : IManager
+    {
+        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager _graphics;
+        private Game _game { get; set; }
+
+        public virtual void Initialize(Game game)
+        {
+            _game = game ?? throw new ArgumentNullException(nameof(game));
+        }
+
+        public virtual void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            //TODO
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            //TODO
+        }
+
+    }
+}
---
+++ src/Managers/WorldManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cascade.src.Managers
+{
+    internal class WorldManager : AbstractManager
+    {
+        public override void Initialize(Game game)
+        {
+            base.Initialize(game);
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
